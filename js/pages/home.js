309f6421a676ecbd02460faf2f05d1b2d94b31e6
karlwoien <karlwoien@gmail.com>
2023-11-08 17:36:30 +0100

    Updated functions to render products on home-page

--- js/pages/home.js
+++ js/pages/home.js
@@ -1,7 +1,27 @@
 import { getProducts } from "../api/products.js";
-import { renderNews } from "../render/home.js";
+import { renderNewArrivals } from "../render/home.js";
 
-export async function home() {
-    const products = await getProducts();
-    renderNews(products);
+export function home () {
+    // fetch products for discover new arrivals
+    let products;
+    let news = [];
+
+    async function getNewArrivals () {
+
+        products = await getProducts ();
+
+        for (let i = 0; i < products.length; i++) {
+            if (news.length < 3 && products[i].onSale === false) {
+                news.push(products[i]);
+            }
+        }
+
+        // Create discover new arrivals
+
+        const newArrivals = document.querySelector(".discover-row")
+        newArrivals.innerHTML = renderNewArrivals(news)
+        
+    }
+
+    getNewArrivals()
 }
--- js/render/home.js
+++ js/render/home.js
@@ -1,31 +1,43 @@
-export function renderNewArrivals(productData) {
-    const newArrivals = document.createElement("a");
-    newArrivals.href = "/specific-product.html?id=" + productData.id;
+export function renderNewArrivals (news) {
+
+    //Create container
+    let newArrivals = document.createElement ("div");
+    newArrivals.classList.add ("discover-row");
+
+    //discover new arrivals
+
+    for (let i = 0; i < news.length; i++) {
+        let product = document.createElement("div");
+
+        let container = document.createElement("a");
+        container.classList.add("discover-column");
+        container.href = `/specific-product.html?id=${news[i].id}`;
+
+        let image = document.createElement("img");
+        image.src = news[i].image;
+        image.alt = news[i].title;
+
+        let title = document.createElement("h3");
+        title.innerHTML = news[i].title;
+
+        let price = document.createElement("p");
+        price.innerHTML = "$ " + news[i].price;
+
+        const button = document.createElement ("button");
+        button.innerText = "shop now";
+        button.classList.add ("btn--red")
+        newArrivals.append(button);
+
+        
+        container.append(image)
+        container.append(title)
+        container.append(price)
+        container.append(button)        
     
-    const img = document.createElement ("img");
-    img.src = productData.image;
-    img.alt = productData.title;
-    img.style.height = "400px";
-    newArrivals.append(img);
+        product.append(container)
 
-    const productTitle = document.createElement("h3");
-    productTitle.innerText = productData.title;
-    newArrivals.append(productTitle);
-
-    const productPrice = document.createElement("p");
-    productPrice.innerText = "$ " + productData.price;
-    newArrivals.append(productPrice);
-
-    const btn = document.createElement("button");
-    btn.innerText = "shop now"
-    btn.classList.add("btn--red");
-    newArrivals.append(btn)
-
-    document.querySelector(".discover-row").append(newArrivals);
-
+        newArrivals.append(product)
+    }
+    
+    return newArrivals.innerHTML;
 }
-
-export function renderNews (newArrivals) {
-    newArrivals.forEach(renderNewArrivals);
-}
-
