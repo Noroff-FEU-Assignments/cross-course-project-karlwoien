99f85d1f880424e5708fb0740446762f0eb16671
karlwoien <karlwoien@gmail.com>
2023-11-10 18:01:54 +0100

    error message

---
+++ js/components/errorMessage.js
@@ -0,0 +1,7 @@
+
+export function  displayError (message) {
+    
+    return `<div class="error">
+                <p class="error-message">${message}</p>
+            </div>`
+}   
--- js/pages/home.js
+++ js/pages/home.js
@@ -1,6 +1,7 @@
 import { getProducts } from "../api/products.js";
 import { renderNewArrivals } from "../render/home.js";
 import { loader } from "../constants.js";
+import { displayError } from "../components/errorMessage.js";
 
 export function home () {
     // fetch products for discover new arrivals
@@ -9,18 +10,21 @@ export function home () {
 
     async function getNewArrivals () {
 
-        products = await getProducts ();
+        try {
+            products = await getProducts ();
 
-        for (let i = 0; i < products.length; i++) {
-            if (discoverNewArrivals.length < 3 && products[i].onSale === false) {
-                discoverNewArrivals.push(products[i]);
+            for (let i = 0; i < products.length; i++) {
+                if (discoverNewArrivals.length < 3 && products[i].onSale === false) {
+                    discoverNewArrivals.push(products[i]);
+                }
+        
+            const newArrivals = document.querySelector(".discover-row")
+            newArrivals.innerHTML = renderNewArrivals(discoverNewArrivals)
             }
+        } catch (error) {
+            const newArrivals = document.querySelector(".discover-row")
+            newArrivals.innerHTML = displayError ("Error! " + "Unable to access New Arrivals. Please try again later.");
         }
-
-        // Create discover new arrivals
-
-        const newArrivals = document.querySelector(".discover-row")
-        newArrivals.innerHTML = renderNewArrivals(discoverNewArrivals)
         
     }
     getNewArrivals()
