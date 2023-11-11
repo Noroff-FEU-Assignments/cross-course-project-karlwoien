41ddc6f033980ee950f16ce8ec6c1b17e5edd9c4
karlwoien <karlwoien@gmail.com>
2023-11-11 18:06:20 +0100

    Update specific-product.js

--- js/pages/specific-product.js
+++ js/pages/specific-product.js
@@ -1,6 +1,9 @@
 import { getProduct } from "../api/products.js";
 import { renderSpecificProduct } from "../render/specific-product.js";
 import { loader } from "../constants.js";
+import { changeTitle } from "../components/changeTitle.js";
+
+let product;
 
 export async function specificProduct() {
   try {
@@ -9,6 +12,11 @@ export async function specificProduct() {
 
     const product = await getProduct(id);
     renderSpecificProduct(product);
+    changeTitle (
+      `${product.title
+        .replace ("Rainy Days ", "")
+        .replace ("Product", "")} | Rainy Days`
+    );
     loader.classList.remove("loader");
   }
   catch (error) {
