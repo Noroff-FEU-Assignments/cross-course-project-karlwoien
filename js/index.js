b05f78e60372a3662c59fa7447399b2cc51c7e94
karlwoien <karlwoien@gmail.com>
2023-11-12 11:36:38 +0100

    changing pathname so that netlify will work

--- js/index.js
+++ js/index.js
@@ -2,14 +2,14 @@ import { home } from "/js/pages/home.js";
 import { specificProduct } from "./pages/specific-product.js";
 import { shopProducts } from "./pages/products.js";
 
-if (location.pathname === "/shop.html") {
+if (location.pathname === "/shop") {
     shopProducts()
 }
 
-if (location.pathname === "/specific-product.html") { 
+if (location.pathname === "/specific-product") { 
     specificProduct()
 }
 
-if (location.pathname === "/index.html") { 
+if (location.pathname === "/") { 
     home()
 }
