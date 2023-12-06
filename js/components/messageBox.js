6d02b9ecdcd9b65df823c8d8f64ba782987c6e7d
karlwoien <karlwoien@gmail.com>
2023-12-06 17:36:16 +0100

    update styling modal/message box

--- js/components/messageBox.js
+++ js/components/messageBox.js
@@ -18,9 +18,9 @@ closeButton.onclick = function () {
     modal.style.display = "none";
 }
 
-window.onclick = function (e) {
-    if (e.target == modal) {
+/*window.onclick = function (e) {
+    if (e.target = modal) {
         modal.style.display = "none";
     }
-}
+}*/
 }
