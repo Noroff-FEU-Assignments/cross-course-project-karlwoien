2795c5bd14683934c41fbdbe9529bad918a807aa
karlwoien <karlwoien@gmail.com>
2023-12-07 11:49:51 +0100

    styling and function to select payment method

---
+++ js/components/paymentSelection.js
@@ -0,0 +1,12 @@
+export function selectPaymentMethod () {
+    const paymentMethod = document.querySelectorAll (`.payment-method-choice`);
+
+    paymentMethod.forEach ((method) => {
+        method.addEventListener ("click", function(){
+            paymentMethod.forEach ((otherMethod) => {
+                otherMethod.classList.remove ("payment-method-choice-active");
+            });
+            this.classList.add ("payment-method-choice-active");
+        })
+    })
+}
